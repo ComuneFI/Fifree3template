bf1e46e579d3195fbe5ce7d2ee07891894f07e7c
d59495 <andrea.manzi@comune.fi.it>
2018-04-12 15:21:43 +0200

    aggiunto autenticazionebundle

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Fi\PannelloAmministrazioneBundle\PannelloAmministrazioneBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Fi\AutenticazioneBundle\AutenticazioneBundle::class => ['all' => true],
 ];
